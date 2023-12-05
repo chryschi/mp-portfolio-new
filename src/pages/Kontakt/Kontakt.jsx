0be945a6c7bcebc7ee55b80ef00f466e602df24e
chryschi <121822197+chryschi@users.noreply.github.com>
2023-12-05 11:03:36 +0100

    Setup HTML structure for Kontakt page

--- src/pages/Kontakt/Kontakt.jsx
+++ src/pages/Kontakt/Kontakt.jsx
@@ -1,8 +1,41 @@
 const Kontakt = () => {
+  const contacts = [
+    {
+      label: "Überrasch mich mit einem Anruf",
+      linkContent: "+49 (0) 159 04 233 041",
+    },
+    {
+      label: "Oder per EMail",
+      linkContent: "prechtl.marius@googlemail.com",
+    },
+    {
+      label: "Folge mir auf Pinterest",
+      linkContent: "mariusprechtl",
+    },
+    {
+      label: "Und auf Instagram",
+      linkContent: "mariusprechtl",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Hi! Kontaktiert mich hier!</h1>
-    </div>
+    <main>
+      <h1>
+        Zeitlose und minimalistische Architektur für jeden. Let's do this. Da
+        muss mir noch irgendwas tolles einfallen, was man hier hinschreiben
+        kann.
+      </h1>
+      <ul>
+        {contacts.map((entry) => (
+          <li>
+            <h3>{entry.label}</h3>
+            <p>
+              <a>{entry.linkContent}</a>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 };
 
