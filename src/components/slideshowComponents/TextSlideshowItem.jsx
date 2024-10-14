efb4b0c5fd278cc2e2826544306f8089e1cdc8e1
chryschi <121822197+chryschi@users.noreply.github.com>
2024-10-14 16:35:48 +0200

    Restore desired layout of slideshow

--- src/components/slideshowComponents/TextSlideshowItem.jsx
+++ src/components/slideshowComponents/TextSlideshowItem.jsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
-import { useRef } from "react";
-import { useState } from "react";
+// import { useEffect } from "react";
+// import { useRef } from "react";
+// import { useState } from "react";
 
 const TextSlideshowItem = ({ content }) => {
   //KEEP FOR CUSTOM SCROLLBAR
